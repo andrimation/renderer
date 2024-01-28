f70cf5c742c22f5be004b8e473cff3f805070a02
andrimation <88882438+andrimation@users.noreply.github.com>
2024-01-28 02:30:18 +0100

    - renderer update.
    ray tracing correct perspective + ray trianfle intersection fixed

--- 3dFunctions.h
+++ 3dFunctions.h
@@ -1,16 +1,56 @@
 #pragma once
+#include <iostream>
+#include <fstream>
 
 struct vec3d
 {
 	float x, y, z, w;
 
-	vec3d operator + (const vec3d& vOther)
+	//vec3d() {};
+	//vec3d(float xin, float yin, float zin) : x(xin), y(yin), z(zin) {};
+	//vec3d(float xin, float yin, float zin, float win) : x(xin), y(yin), z(zin), w(win) {};
+	//~vec3d() {};
+
+	vec3d operator + (const vec3d& vOther) const
 	{
 		vec3d newVec;
 		newVec.x = this->x + vOther.x;
 		newVec.y = this->y + vOther.y;
 		newVec.z = this->z + vOther.z;
 		return newVec;
+	}
+
+	vec3d operator + (const float f)
+	{
+		vec3d newVec;
+		newVec.x = this->x + f;
+		newVec.y = this->y + f;
+		newVec.z = this->z + f;
+		return newVec;
+	}
+
+	vec3d operator += (const vec3d& vOther)
+	{
+		this->x += vOther.x;
+		this->y += vOther.y;
+		this->z += vOther.z;
+		return *this;
+	}
+
+	vec3d operator += (const float& vOther)
+	{
+		this->x += vOther;
+		this->y += vOther;
+		this->z += vOther;
+		return *this;
+	}
+
+	vec3d operator - ()
+	{
+		this->x = -this->x;
+		this->y = -this->y;
+		this->z = -this->z;
+		return *this;
 	}
 	
 	vec3d operator - (const vec3d& vOther)
@@ -22,12 +62,30 @@ struct vec3d
 		return newVec;
 	}
 
-	vec3d operator * (const vec3d& vOther)
+	vec3d operator - (const float f)
+	{
+		vec3d newVec;
+		newVec.x = this->x - f;
+		newVec.y = this->y - f;
+		newVec.z = this->z - f;
+		return newVec;
+	}
+
+	vec3d operator * (const vec3d& vOther) const
 	{
 		vec3d newVec;
 		newVec.x = this->x * vOther.x;
 		newVec.y = this->y * vOther.y;
 		newVec.z = this->z * vOther.z;
+		return newVec;
+	}
+
+	vec3d operator * (const float val) const
+	{
+		vec3d newVec;
+		newVec.x = this->x * val;
+		newVec.y = this->y * val;
+		newVec.z = this->z * val;
 		return newVec;
 	}
 
@@ -40,6 +98,14 @@ struct vec3d
 		return newVec;
 	}
 
+	vec3d operator /= (const float divide)
+	{
+		this->x /= divide;
+		this->y /= divide;
+		this->z /= divide;
+		return *this;
+	}
+
 	static float VectorDotProduct(vec3d& vec1, vec3d& vec2)
 	{
 		return vec1.x * vec2.x + vec1.y * vec2.y + vec1.z * vec2.z;
@@ -48,6 +114,11 @@ struct vec3d
 	static float VectorLenght(vec3d& v1)
 	{
 		return sqrtf(VectorDotProduct(v1, v1));
+	}
+
+	float VectorLenght_squared() const
+	{
+		return this->x * this->x + this->y * this->y + this->z * this->z;
 	}
 
 	static vec3d VectorNormalize(vec3d& vec1)
@@ -72,10 +143,16 @@ struct vec3d
 
 };
 
+inline static std::ostream& operator << (std::ostream& out, const vec3d v)
+{
+	return out << v.x << " " << v.y << " " << v.z << "\n";
+}
+
 struct triangle
 {
 	vec3d points[3];
 	vec3d middlePoint;
+	vec3d triangleNormal;
 
 	WCHAR colorChar;
 	WORD colorAtrr;
@@ -98,17 +175,10 @@ struct mat4x4
 	float m[4][4] = {0};
 };
 
-class camera 
-{
-public:
-	vec3d position = { 0.0f,0.0f,0.0f,0.0f };
-	
-};
-
 class lightSource
 {
 public:
-	vec3d dir = { 0.0f,0.0f,-1.0f,0.0f };
+	vec3d dir = { 0.0f,-0.5f,-1.0f,0.0f };
 };
 
 void LoadMeshes(std::vector<string>& meshesToLoad,std::vector<mesh>& sceneMeshes)
@@ -199,8 +269,9 @@ void createProjectionMatrix(mat4x4& matrix,int screenHeight,int screenWidth)
 	matrix.m[3][3] = 0.0f;
 }
 
-void createRotationZMatrix(mat4x4& matrix, float time,float dir)
+mat4x4 createRotationZMatrix(float time,float dir)
 {
+	mat4x4 matrix;
 	float multiplier = time * dir;
 	matrix.m[0][0] = cosf(multiplier);
 	matrix.m[0][1] = sinf(multiplier);
@@ -208,6 +279,7 @@ void createRotationZMatrix(mat4x4& matrix, float time,float dir)
 	matrix.m[1][1] = cosf(multiplier);
 	matrix.m[2][2] = 1.0;
 	matrix.m[3][3] = 1.0;
+	return matrix;
 }
 
 mat4x4 createRotationXMatrix(float angle)
@@ -269,4 +341,182 @@ CHAR_INFO GetColour(float lum)
 }
 
 
+// RayTracer 
+
+class ray
+{
+public:
+	ray() {}
+	ray(vec3d& origin, vec3d& direction) : orig(origin), dir(direction) {}
+	~ray() {};
+
+	vec3d orig;
+	vec3d dir;
+
+	vec3d at(float t) const 
+	{ 
+		return  (dir * t) + orig; 
+	}
+	
+};
+
+class camera
+{
+public:
+	float focalLength = 1.0;
+	vec3d position = { 0.0f,0.0f,0.0f,0.0f };
+
+};
+
+class RayTracer
+{
+public:
+	RayTracer(camera camIn) : cam(camIn), imageWidth(1), imageHeight(1) {};
+	~RayTracer() {};
+
+	float imageWidth;
+	int imageHeight;
+	camera cam;
+
+	float viewportHeight = 0.01f;
+	float viewportWidth  = 0.01f; 
+
+	vec3d viewportU = { viewportWidth,0.0,0.0 };
+	vec3d viewportV = { 0.0,viewportHeight,0.0 };
+
+	vec3d pixelDeltaU = viewportU / imageWidth;
+	vec3d pixelDeltaV = viewportV / imageHeight;
+
+	vec3d focalLenght{ 0.0,0.0,cam.focalLength };
+
+	vec3d viewportTopLeft = cam.position + focalLenght - (viewportU / 2) - (viewportV / 2);
+	vec3d pixel00 = viewportTopLeft + pixelDeltaU / 2 + pixelDeltaV / 2;
+
+	void RenderToFile(std::string fileName, std::vector<triangle> trianglesToCheck)
+	{
+		std::ofstream renderResultFile(fileName, std::ios::binary);
+
+		renderResultFile << "P3\n" << imageWidth << " " << imageHeight << "\n255\n";
+
+		for (int j = -imageHeight*0.5; j < imageHeight*0.5; j++)
+		{
+			for (int i = -imageWidth*0.5; i < imageWidth*0.5; i++)
+			{	
+				vec3d nextPixel = pixel00 + (pixelDeltaU * i) + (pixelDeltaV * j);
+				auto rayDir = nextPixel - cam.position;
+
+				ray r(cam.position, rayDir);
+
+				vec3d pixelCol = rayColor(r);
+
+				for (triangle& triToTest : trianglesToCheck)
+				{								
+					if (rayHitTriangle(triToTest, r))
+					{
+						pixelCol = vec3d{ 0.0,0.0,0.0 };
+						break;
+					}					
+				}			
+				writeColor(renderResultFile, pixelCol);			
+			}
+		}
+
+		renderResultFile.close();
+
+	}
+
+	void writeColor(std::ostream& out, vec3d pixelColor)
+	{
+		out << static_cast<int>(255.999 * pixelColor.x) << " "
+			<< static_cast<int>(255.999 * pixelColor.y) << " "
+			<< static_cast<int>(255.999 * pixelColor.z) << "\n";
+	}
+
+	vec3d rayColor(const ray& r)
+	{	
+		// Gradient t³a
+		vec3d direction = r.dir;
+		vec3d normalized_ray = vec3d::VectorNormalize(direction);
+		auto a = 0.5 * ((normalized_ray.y) + 1.0);
+
+		vec3d colorA{ 1.0,1.0,1.0 };
+		vec3d colorB{ 0.5,0.7,1.0 };
+
+		return colorA*(1.0-a) + colorB*a;
+	}
+
+	bool rayHitTriangle(triangle& triToTest, ray& r)
+	{
+		// 1) Najpierw liczymy czy promieñ przecina p³aszczyznê wyznaczon¹ przez normal i puntk na p³aszczyŸnie ( wierzcho³ek trójk¹ta )
+		// punkt przeciêcia promienia i p³aszczyzny obliczê po prostu przez wsadzienie wyniku w ray.at()
+		// 2) Obliczamy czy proimeñ trafi³ w trójk¹t -> iterujemy przez "wektory-boki" trójk¹ta i liczymy CrossProduct z punktu przeciêcia i aktualnego boku
+		// 3) PóŸniej liczymy dotproduct z uzyskanego w pkt 2) nowego wektora i normala trójk¹ta i jeœli wynik jest >= 0 to ray wpada w trójk¹t
+		
+		ray rr = r;
+
+		vec3d triLine1 = triToTest.points[1] - triToTest.points[0];
+		vec3d triLine2 = triToTest.points[2] - triToTest.points[0];
+
+		vec3d normal = vec3d::VectorCrossProduct(triLine1, triLine2);
+		triToTest.triangleNormal = normal;
+
+		float NdotRayDirection = vec3d::VectorDotProduct(triToTest.triangleNormal, r.dir);
+
+		if (fabs(NdotRayDirection) < FLT_EPSILON)
+		{
+			return false;
+		}
+
+		// Liczenie tego tu to b³¹d, bo trójk¹ty s¹ przesuniête ?
+		/*vec3d triLine1 = triToTest.points[1] - triToTest.points[0];
+		vec3d triLine2 = triToTest.points[2] - triToTest.points[0];
+		vec3d TriNorma = vec3d::VectorCrossProduct(triLine1, triLine2);
+		vec3d TriNormaNormalized = vec3d::VectorNormalize(TriNorma);*/
+
+		vec3d minNorm = triToTest.triangleNormal * - 1;
+
+		float d = vec3d::VectorDotProduct(triToTest.triangleNormal, triToTest.points[0]);
+		float t = -((vec3d::VectorDotProduct(triToTest.triangleNormal, r.orig) - d) / NdotRayDirection);
+
+		if (t <= 0)
+		{
+			return false;
+		}
+
+		vec3d P = r.orig + (r.dir*t);
+
+		vec3d edge1 = triToTest.points[1] - triToTest.points[0];
+		vec3d interscectionVec1 = P - triToTest.points[0];
+		// Najpierw cross product. Jako ze P jest na p³aszczyŸnie trójk¹ta, to cross product mo¿e mieæ tylko dwa zwroty "do" albo "od" p³aszczyzny,
+		// w zale¿noœci od po³o¿enia P wzglêdem edge1. To pozwala na sprawdzenie póŸniej dot productu.
+		vec3d test1 = vec3d::VectorCrossProduct(edge1, interscectionVec1);
+		float testDot1 = vec3d::VectorDotProduct(test1, triToTest.triangleNormal);
+		// szybciej sprawdzaæ tu ni¿ na koñcu.
+		if (testDot1 < 0.0)
+		{
+			return false;
+		}
+
+		vec3d edge2 = triToTest.points[2] - triToTest.points[1];
+		vec3d interscectionVec2 = P - triToTest.points[1];
+		vec3d test2 = vec3d::VectorCrossProduct(edge2, interscectionVec2);
+		float testDot2 = vec3d::VectorDotProduct(test2, triToTest.triangleNormal);
+		if (testDot2 < 0.0)
+		{
+			return false;
+		}
+
+		vec3d edge3 = triToTest.points[0] - triToTest.points[2];
+		vec3d interscectionVec3 = P - triToTest.points[2];
+		vec3d test3 = vec3d::VectorCrossProduct(edge3, interscectionVec3);
+		float testDot3 = vec3d::VectorDotProduct(test3, triToTest.triangleNormal);
+		if (testDot3 < 0.0)
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+};
 
--- main.cpp
+++ main.cpp
@@ -10,16 +10,20 @@ class consoleEngine : public olcConsoleGameEngine
 public:
 // Data
 
-	std::vector<string> meshesToLoad = { "VideoShip.obj","plane.obj"};
+	std::vector<string> meshesToLoad = { "plane.obj","VideoShip2.obj" };
 	std::vector<mesh> sceneMeshes;
 	mat4x4 projectionMatrix;
 	camera camView;
 	lightSource light;
 
+	bool ifRenderImage = false;
+
 	float timeZ = 0;
 	float timeX = 0;
 	float timeY = 0;
 
+	RayTracer renderer{ camView };
+	
 // Functions
 	consoleEngine()
 	{
@@ -30,6 +34,8 @@ public:
 	{
 		LoadMeshes(meshesToLoad, sceneMeshes);	
 		createProjectionMatrix(projectionMatrix,ScreenHeight(),ScreenWidth());
+		renderer.imageWidth = (int)ScreenWidth();
+		renderer.imageHeight = (int)ScreenHeight();
 		return true;
 	}
 
@@ -41,12 +47,11 @@ public:
 		mat4x4 matXRot;
 		mat4x4 matYRot;
 
-		
-
-		createRotationZMatrix(matZRot, timeZ, 1);
+		matZRot = createRotationZMatrix(timeZ,1);
 		matXRot = createRotationXMatrix(timeX);
 		matYRot = createRotationYMatrix(timeY);
 
+		std::vector<triangle> TrianglesToRayTrace;
 
 		for (int i = 0; i < sceneMeshes.size(); i++)
 		{
@@ -69,22 +74,29 @@ public:
 			{
 				timeY -= 5.0 * FElapsedTime;
 			}
+			if (GetKey(VK_SPACE).bHeld)
+			{
+				ifRenderImage = true;
+			}
 
 			std::vector<triangle> TrianglesToSort;
 					
 			for (triangle& tri : meshToProject.meshTriangles)
 			{
-				
 				triangle TriRotZ,TriRotX,TriRotY,TriOffset,TriProj;
 
-				if (i == 0)
+				if (i == 1)
 				{
-					MultiplyMatrixTriangle(matZRot, tri, TriRotZ);
-					MultiplyMatrixTriangle(matXRot, TriRotZ, TriRotX);
-					MultiplyMatrixTriangle(matYRot, TriRotX, TriRotY);
+					//MultiplyMatrixTriangle(matZRot, tri, TriRotZ);
+					//MultiplyMatrixTriangle(matXRot, tri, TriRotX);
+					MultiplyMatrixTriangle(matYRot, tri, TriRotY);
 				}
 				else
 				{
+					//MultiplyMatrixTriangle(matZRot, tri, TriRotZ);
+					//MultiplyMatrixTriangle(matXRot, TriRotZ, TriRotX);
+					//MultiplyMatrixTriangle(matYRot, TriRotX, TriRotY);
+
 					TriRotY = tri;
 				}
 
@@ -104,37 +116,81 @@ public:
 				vec3d triangleNormalNormalized = vec3d::VectorNormalize(triangleNormalVector);
 
 				float cameraTriangleDotProduct = vec3d::VectorDotProduct(camTriangleVectorNormalized, triangleNormalNormalized);
+						
+				vec3d lightVectorNormalized = vec3d::VectorNormalize(light.dir);
+				float triLightDotProduct = vec3d::VectorDotProduct(triangleNormalNormalized, lightVectorNormalized);
+
+				CHAR_INFO color = GetColour(triLightDotProduct);
+
+				MultiplyMatrixTriangle(projectionMatrix, TriOffset, TriProj);
+				//TriProj = TriOffset;
+
+				TriProj.points[0].x += 1.0f;
+				TriProj.points[0].y += 1.0f;
+				TriProj.points[1].x += 1.0f;
+				TriProj.points[1].y += 1.0f;
+				TriProj.points[2].x += 1.0f;
+				TriProj.points[2].y += 1.0f;
+
+				TriProj.points[0].x *= (float)ScreenWidth() * 0.5f;
+				TriProj.points[0].y *= (float)ScreenHeight() * 0.5f;
+				TriProj.points[1].x *= (float)ScreenWidth() * 0.5f;
+				TriProj.points[1].y *= (float)ScreenHeight() * 0.5f;
+				TriProj.points[2].x *= (float)ScreenWidth() * 0.5f;
+				TriProj.points[2].y *= (float)ScreenHeight() * 0.5f;
+
 				
-				if (cameraTriangleDotProduct < 0.0f )
+
+				// Ok czyli wiem ¿e problem jest gdzieœ tu. Jeœli dajê do renderowania TriOffset - a wiêc bez matrycy projekcji,
+				// to wtedy renderuje siê poprawnie
+				
+				triangle Xtri = TriOffset;
+
+				Xtri.points[0].x += 1.0f;
+				Xtri.points[0].y += 1.0f;
+				Xtri.points[1].x += 1.0f;
+				Xtri.points[1].y += 1.0f;
+				Xtri.points[2].x += 1.0f;
+				Xtri.points[2].y += 1.0f;
+
+				Xtri.points[0].x *= (float)ScreenWidth() * 0.5f;
+				Xtri.points[0].y *= (float)ScreenHeight() * 0.5f;
+				Xtri.points[1].x *= (float)ScreenWidth() * 0.5f;
+				Xtri.points[1].y *= (float)ScreenHeight() * 0.5f;
+				Xtri.points[2].x *= (float)ScreenWidth() * 0.5f;
+				Xtri.points[2].y *= (float)ScreenHeight() * 0.5f;
+
+				TriProj.colorChar = color.Char.UnicodeChar;
+				TriProj.colorAtrr = color.Attributes;
+				TriProj.triangleNormal = triangleNormalNormalized;
+
+				TriProj.middlePoint = TriProj.ComputeTriangleMiddle();
+
+				if (i == 1) { TrianglesToRayTrace.push_back(TriOffset); }
+				
+
+				if (cameraTriangleDotProduct < 0.0f)
 				{
-					vec3d lightVectorNormalized = vec3d::VectorNormalize(light.dir);
-					float triLightDotProduct = vec3d::VectorDotProduct(triangleNormalNormalized, lightVectorNormalized);
-
-					CHAR_INFO color = GetColour(triLightDotProduct);
-
-					MultiplyMatrixTriangle(projectionMatrix, TriOffset, TriProj);
-
-					TriProj.points[0].x += 1.0f;
-					TriProj.points[0].y += 1.0f;
-					TriProj.points[1].x += 1.0f;
-					TriProj.points[1].y += 1.0f;
-					TriProj.points[2].x += 1.0f;
-					TriProj.points[2].y += 1.0f;
-
-					TriProj.points[0].x *= (float)ScreenWidth() * 0.5f;
-					TriProj.points[0].y *= (float)ScreenHeight() * 0.5f;
-					TriProj.points[1].x *= (float)ScreenWidth() * 0.5f;
-					TriProj.points[1].y *= (float)ScreenHeight() * 0.5f;
-					TriProj.points[2].x *= (float)ScreenWidth() * 0.5f;
-					TriProj.points[2].y *= (float)ScreenHeight() * 0.5f;
-
-					TriProj.colorChar = color.Char.UnicodeChar;
-					TriProj.colorAtrr = color.Attributes;
-
-					TriProj.middlePoint = TriProj.ComputeTriangleMiddle();
-
-					TrianglesToSort.push_back(TriProj);				
+					if (i == 1)
+					{
+						TrianglesToSort.push_back(TriProj);
+						
+						//std::cout << "Triangle: " << TriProj.points[0] << "  " << TriProj.points[1] << "  " << TriProj.points[2];
+					}
+					else
+					{
+						FillTriangle(TriProj.points[0].x, TriProj.points[0].y,
+							TriProj.points[1].x, TriProj.points[1].y, TriProj.points[2].x,
+							TriProj.points[2].y, PIXEL_SOLID, FG_GREY);
+						DrawTriangle(TriProj.points[0].x, TriProj.points[0].y,
+							TriProj.points[1].x, TriProj.points[1].y, TriProj.points[2].x,
+							TriProj.points[2].y, PIXEL_SOLID, FG_BLACK);
+						
+					}
+					
 				}
+
+				
 			}
 			// Posortowaæ listê trójk¹tów wed³ug odleg³oœci.
 			std::sort(TrianglesToSort.begin(), TrianglesToSort.end(), [&](triangle a, triangle b) {
@@ -143,14 +199,26 @@ public:
 				return a.middlePoint.z < b.middlePoint.z;
 				});
 
+			/*std::sort(TrianglesToRayTrace.begin(), TrianglesToRayTrace.end(), [&](triangle a, triangle b) {
+				vec3d vec1 = a.middlePoint - camView.position;
+				vec3d vec2 = b.middlePoint - camView.position;
+				return a.middlePoint.z < b.middlePoint.z;
+				});*/
+
 			for (triangle& triToDraw : TrianglesToSort)
 			{
 				FillTriangle(triToDraw.points[0].x, triToDraw.points[0].y,
 					triToDraw.points[1].x, triToDraw.points[1].y, triToDraw.points[2].x,
 					triToDraw.points[2].y, triToDraw.colorChar, triToDraw.colorAtrr);
-				DrawTriangle(triToDraw.points[0].x, triToDraw.points[0].y,
+				/*DrawTriangle(triToDraw.points[0].x, triToDraw.points[0].y,
 					triToDraw.points[1].x, triToDraw.points[1].y, triToDraw.points[2].x,
-					triToDraw.points[2].y, PIXEL_SOLID,FG_BLACK);
+					triToDraw.points[2].y, PIXEL_SOLID,FG_WHITE);*/
+			}
+
+			if (ifRenderImage)
+			{			
+				renderer.RenderToFile("plik.ppm", TrianglesToRayTrace);
+				ifRenderImage = false;
 			}
 		}
 
@@ -163,9 +231,10 @@ int main()
 
 	consoleEngine engineObj;
 
-	if (engineObj.ConstructConsole(512, 512, 4, 4))
+	if (engineObj.ConstructConsole(256, 256, 4, 4))
 	{
 		engineObj.Start();
+
 	}
 
 	return 0;
